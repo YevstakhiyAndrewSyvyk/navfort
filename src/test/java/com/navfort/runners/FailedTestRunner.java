22b59c979938f8a2e16f3ba36c46039f6f8c06d5
mengliiii <menglivy@hotmail.com>
2023-02-25 14:52:13 -0500

    add FailedTestRunner

---
+++ src/test/java/com/navfort/runners/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package com.navfort.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/navfort/step_definitions"
+)
+public class FailedTestRunner {
+
+}
