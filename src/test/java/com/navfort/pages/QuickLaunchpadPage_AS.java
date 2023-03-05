f57f0c3ef640904249d391f3ace65ac44a537d1a
Andrew :) <yevstakhiy.syvyk@gmail.com>
2023-03-05 17:15:58 +0200

    Creating separate pages for my US07

---
+++ src/test/java/com/navfort/pages/QuickLaunchpadPage_AS.java
@@ -0,0 +1,36 @@
+package com.navfort.pages;
+
+import com.navfort.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class QuickLaunchpadPage_AS {
+    public QuickLaunchpadPage_AS() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(className = "oro-subtitle")
+    public WebElement pageHeader;
+
+    @FindBy(xpath = "//div[@id='main-menu']/ul/li")
+    public List<WebElement> main_menu;
+
+    @FindBy(xpath = "//a[@href='javascript: void(0);']")
+    public WebElement txt_username_profile;
+
+    @FindBy(linkText = "Logout")
+    public WebElement Logout;
+
+    @FindBy(xpath = "(//span[@class='title title-level-1'])[2]")
+    public WebElement FleetDropdown;
+
+    @FindBy (xpath = "//span[.='Vehicles']")
+    public WebElement VehiclesPageLink;
+
+
+
+
+}
