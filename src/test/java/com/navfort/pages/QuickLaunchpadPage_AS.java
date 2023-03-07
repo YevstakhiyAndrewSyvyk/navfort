0501f1e52d500cbe928e4af9410aa4983a812bee
Andrew :) <yevstakhiy.syvyk@gmail.com>
2023-03-07 23:41:02 +0200

    Revert "Adding US02 Feature. It's stable"
    This reverts commit d1cc18050432d44e332846be0a3ce215fbe5ee4a.

--- src/test/java/com/navfort/pages/QuickLaunchpadPage_AS.java
+++ src/test/java/com/navfort/pages/QuickLaunchpadPage_AS.java
@@ -30,9 +30,6 @@ public class QuickLaunchpadPage_AS {
     @FindBy (xpath = "//span[.='Vehicles']")
     public WebElement VehiclesPageLink;
 
-    @FindBy (xpath = "//i[@title='Get help']")
-    public WebElement GetHelpButton;
-
 
 
 
