d1cc18050432d44e332846be0a3ce215fbe5ee4a
Andrew :) <yevstakhiy.syvyk@gmail.com>
2023-03-07 23:40:46 +0200

    Adding US02 Feature. It's stable

--- src/test/java/com/navfort/pages/QuickLaunchpadPage_AS.java
+++ src/test/java/com/navfort/pages/QuickLaunchpadPage_AS.java
@@ -30,6 +30,9 @@ public class QuickLaunchpadPage_AS {
     @FindBy (xpath = "//span[.='Vehicles']")
     public WebElement VehiclesPageLink;
 
+    @FindBy (xpath = "//i[@title='Get help']")
+    public WebElement GetHelpButton;
+
 
 
 
