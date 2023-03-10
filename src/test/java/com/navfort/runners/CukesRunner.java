32a98f2bb965459ecdb1a76d7dd69f57dba007c2
Andrew :) <113801790+YevstakhiyAndrewSyvyk@users.noreply.github.com>
2023-03-10 17:03:05 +0200

    Update CukesRunner.java
    Cleaning tags

--- src/test/java/com/navfort/runners/CukesRunner.java
+++ src/test/java/com/navfort/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/navfort/step_definitions",
         dryRun = false,
-        tags = "@US04",
+        tags = "",
         publish = true  //Generating a report with public link
 
 
